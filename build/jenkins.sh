2e5936a1052ce79c453c2430c7e4d4e1cff05998
Philippe M. Chiasson <gozer@mozilla.com>
2014-11-13 15:21:23 -0500

    create per-branch repos

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -5,23 +5,31 @@ set -eu
 BUILD_DIR=`dirname $0`
 cd $BUILD_DIR/..
 
+BRANCH=`git symbolic-ref --short HEAD`
+if [ "$BRANCH" == "master" ]; then
+  echo "Can't release from $BRANCH branch"
+  exit 1
+fi
+
+REPO=rpms/$BRANCH
+
 ./build/release.sh --define="rpm_jenkins_job jenkins.$BUILD_NUMBER."
 
-mkdir -p rpms/SRPMS
-mkdir -p rpms/RPMS/noarch
+mkdir -p $REPO/SRPMS
+mkdir -p $REPO/RPMS/noarch
 
-mv *.src.rpm rpms/SRPMS/
-mv *.rpm rpms/RPMS/noarch/
+mv *.src.rpm $REPO/SRPMS/
+mv *.rpm $REPO/RPMS/noarch/
 
-createrepo --revision=$BUILD_NUMBER --update rpms
+createrepo --revision=$BUILD_NUMBER --update $REPO
 
-gpg --export -a jenkins@mozilla.org > rpms/RPM-KEY
+gpg --export -a jenkins@mozilla.org > $REPO/RPM-KEY
 
-cat << EOF > rpms/$JOB_NAME.repo
-[$JOB_NAME]
-name=$JOB_NAME
-baseurl=${JOB_URL}lastSuccessfulBuild/artifact/rpms/
+cat << EOF > $REPO/$JOB_NAME.repo
+[$BRANCH]
+name=$BRANCH
+baseurl=${JOB_URL}lastSuccessfulBuild/artifact/$REPO
 gpgcheck=0
-gpgkey=${JOB_URL}lastSuccessfulBuild/artifact/rpms/RPM-KEY
+gpgkey=${JOB_URL}lastSuccessfulBuild/artifact/$REPO/RPM-KEY
 enabled=1
 EOF
